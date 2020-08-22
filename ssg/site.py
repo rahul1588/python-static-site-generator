4768cedfaaa23f79eeaa1a7f5888618dc9462a3c
Rahul <rahul7751@gmail.com>
2020-08-22 21:41:12 +0530

    part two

--- ssg/site.py
+++ ssg/site.py
@@ -1,13 +1,15 @@
 import sys
+
 from pathlib import Path
 
 
 
 class Site():
 
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
@@ -21,6 +23,18 @@ class Site():
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
 
 
 
